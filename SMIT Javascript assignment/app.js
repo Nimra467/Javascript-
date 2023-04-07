8def25cabe448edd3c3be86875bff4ff920ff9ab
Nimra467 <57403206+Nimra467@users.noreply.github.com>
2023-04-07 20:23:09 +0500

    Update app.js

--- SMIT Javascript assignment/app.js
+++ SMIT Javascript assignment/app.js
@@ -46,7 +46,14 @@ else {
 
 
 // //Question 4
-
+for (let i =0;i<=15;i++){
+    if (i%2===0){
+        console.log(i +" is even.")
+}
+    else {
+        console.log(i +"is odd.")
+    }
+}
 
 //  Question 5
 
