d556471b6ee4a8bec276df44dc4986baf4646712
Nimra467 <57403206+Nimra467@users.noreply.github.com>
2023-04-17 03:50:03 +0500

    Add files via upload

---
+++ SMIT Javascipt assignment 3/app.js
@@ -0,0 +1,52 @@
+(async function(){
+    const response= await fetch("./data.json");
+    const movies=await response.json();
+    console.log(movies)
+   
+const genreInput = document.getElementById('genreInput');
+const ratingInput = document.getElementById('ratingInput');
+const releaseYearInput = document.getElementById('releaseYearInput');
+const btnElem=document.getElementById("recBtn")
+const recList = document.getElementById('recommendationsList');
+const detailElem=document.getElementById("movieDetailContainer");
+
+function movieDetail(movie){
+    detailElem.innerHTML=`
+    <h2>${movie.title}</h2>
+    <ul>${movie.genres.map(function(genre){
+        return "<li>"+ genre +"</li>"
+    }).join("")}</ul>
+    <h3>Rating</h3>
+    <div>${movie.vote_average}</div>
+    <h3>Release date</h3>
+    <div>${movie.release_date}</div>
+    <h3>Language</h3>
+    <div>${movie.original_language}</div>
+    `
+}
+
+function displayMovie(results){
+    recList.innerHTML=""
+results.forEach(function(movie){
+    const li=document.createElement("li");
+    li.innerHTML=movie.title;
+    li.addEventListener("click",function(){
+        movieDetail(movie)
+    })
+    recList.appendChild(li);
+})
+}
+
+
+function search(){
+  const genre = genreInput.value;
+  const rating = Number(ratingInput.value);
+  const releaseYear = Number(releaseYearInput.value);
+const results = movies.filter(function (movie){
+    return (movie.genres.join(" ").toLowerCase().incudes(genre)&& movie.vote_average>=rating && movie.release_date>=releaseYear);
+})
+
+}
+
+btnElem.addEventListener("click",search);
+})();
